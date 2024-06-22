7201b3332a87cf42599a6263274254075dd64e72
nuintun <nuintun@qq.com>
2024-06-22 18:25:12 +0800

    chore: Update code

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,23 @@
 
 import { schema } from './schema';
 import { Options } from './interface';
-import { LoaderDefinition } from 'webpack';
 import { generate, terminate } from './generate';
+import { Compiler, LoaderDefinition } from 'webpack';
 
 export { Options };
 
 const name = __NAME__;
 const initialized = Symbol(name);
 
+declare class ICompiler extends Compiler {
+  [initialized: symbol]: boolean;
+}
+
 export default (function loader(content, sourceMap, additionalData) {
   const callback = this.async();
   const logger = this.getLogger(name);
   const options = this.getOptions(schema);
-
-  const { _compiler: compiler } = this;
+  const compiler = this._compiler as ICompiler;
 
   if (compiler) {
     if (!compiler[initialized]) {
